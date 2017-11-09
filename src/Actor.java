d88f57ecaf2e62b2e72c011a2f7976d27faeddfa
PichvydaTuy <31899661+PichvydaTuy@users.noreply.github.com>
2017-11-09 11:01:07 -0600

    Player and Actor classes created

--- src/Actor.java
+++ src/Actor.java
@@ -1,4 +1,19 @@
-public abstract class Actor
-{
+public abstract class Actor {
+    //currRow: int, currCol: int, data:char)
+    protected int currRow;
+    protected int currCol;
+    protected char data;
+
+    public int getCurrRow(){return currRow;}
+
+    public int getCurrCol(){return currCol;}
+
+    public char getData(){return data;}
+
+
+    public boolean borderCheck();
+
+    public abstract void move(char direction);
+
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,41 @@
 public class Player extends Actor
 {
 
+//    Default constructor to set all the variable of the Player to the
+//    center of the map.
+//        - Write the definition for the move function take in user input and
+//    move ‘P’ up, down, left right base on “awsd” and quit for ‘q’.
+//    Remember to consider border check.
+    public Player(){
+        this.currRow = 4;
+        this.currCol =4;
+        this.data = 'P';
+    }
+
+    public void move(char direction){
+        if(direction == 'w'){
+            currRow =- 1;
+        }
+
+        else if(direction == 's'){
+            currRow =+ 1;
+        }
+
+        else if(direction == 'a'){
+            currCol =- 1;
+        }
+
+        else if(direction == 'd'){
+            currCol =+ 1;
+        }
+
+        else{
+            System.out.println("Invalid move! Please try again.");
+            move(direction);
+        }
+
+
+    }
+
+
 }
